f84a335cffc3bc3faf7f6d42ae4c4f1abbdc1bde
Cole Minor <c.minor@inbox.com>
2014-05-20 15:07:11 -0400

    improve make_continent_parts.sh

--- make_continent_parts.sh
+++ make_continent_parts.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 f=mapinfo.lst
-map_dir=./maps
+map_dir=../maps
+c=Seridia
+s=1.0
+w='.'
 
 die() { echo "$@"; exit 1; }
 
 usage() {
   cat<<EOS
-usage: $0 NAME SCALE
+Usage: $0 OPTIONS
+Options:
+  -i PATH   path to '$f'
+  -c NAME   continent name in '$f'
+  -s FLOAT  divide image sizes by this
+  -w PATH   directory containing rendered maps
 
 NAME is the continent name as it appears
 in the first column of '$f'.
 
-SCALE is a floating point number that the
-image sizes will be divided by. A scale of
+The -s option takes a floating point number that
+the image sizes will be divided by. A scale of
 1.0 implies 1 map tile to 1 pixel.
 EOS
   exit 1
 }
 
-p=$1
-s=$2
-if [ -z "$p" -o -z "$s" ]; then
+if [ $# -eq 0 ]; then
   usage
 fi
 
+args=()
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -c) shift; c="$1" ;;
+    -h|--help) usage ;;
+    -i) shift; f="$1" ;;
+    -s) shift; s="$1" ;;
+    -w) shift; w="$1" ;;
+    *) args+=("$1") ;;
+  esac
+  shift
+done
+
+if [ ${#args[@]} -gt 0 ]; then
+  usage
+fi
+
+if [ ! -f "$f" ]; then
+  die "file not found: '$f'"
+fi
+
 sed -n -e 's,./maps/,,' -e 's,.elm\s*,,' \
-  -e "/^$p[a-z]*\\s*[1-9]/p" "$f" \
+  -e "/^$c[a-z]*\\s*[1-9]/p" "$f" \
   | awk '!/hallo|niege|valen|grotte|_int/ {print $6}' \
   | while read n; do
-  r_map="render_map-$n.png"
+  r_map="$w/render_map-$n.png"
   if [ ! -f "$r_map" ]; then
     die "rendered map '$r_map' not found"
   fi
